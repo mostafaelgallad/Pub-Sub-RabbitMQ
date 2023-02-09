97a11ae83f7dfc683ea8520e9a9e8bde0d9c6ed3
Mostafa El Gallad <m.elgallad@outlook.com>
2023-02-09 10:09:29 +0200

    Add project files.

---
+++ ConsoleApp1/Sender.cs
@@ -0,0 +1,25 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Producer;
+public static class Sender
+{
+    public static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnection())
+        {
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("BasicTest", false, false, false, null);
+                string message = "First rabbitmq Message";
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish("", "BasicTest", null, body);
+                Console.WriteLine("Sent Message {0}.... ", message);
+            }
+        }
+        Console.WriteLine("Press [enter] to exit the sender app");
+        Console.ReadKey();
+    }
+}
